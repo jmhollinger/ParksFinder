fc85af522e1299093372d9aa42f06c6e7a52fc93
jmhollinger <jmholl5@gmail.com>
2015-02-02 15:06:07 -0500

    Add all Parks Data to InfoWindow

--- js/parks-map.js
+++ js/parks-map.js
@@ -25,15 +25,31 @@ function initialize(){
 
   map.data.addListener('click', function(event) {
       
-      if(event.feature.getProperty('basketball') === null ) {var basketball=""} else {var basketball='<li>Basketball Courts:' + event.feature.getProperty('basketball') + '</li>'}
-      if(event.feature.getProperty('baseball') === null ) {var baseball=""} else {var baseball='<li>Baseball Fields:' + event.feature.getProperty('baseball') + '</li>'}
-      if(event.feature.getProperty('tennis') === null ) {var tennis=""} else {var tennis='<li>Tennis Courts:' + event.feature.getProperty('tennis') + '</li>'}
-      if(event.feature.getProperty('volleyball') === null ) {var volleyball=""} else {var volleyball='<li>Volleyball Courts:' + event.feature.getProperty('volleyball') + '</li>'}
-      if(event.feature.getProperty('shelter') === null ) {var shelter=""} else {var shelter='<li>Shelters:' + event.feature.getProperty('shelter') + '</li>'}  
+      if(event.feature.getProperty('center') === null ) {var center=""} else { var center='<li>Community Center</li>'}
+      if(event.feature.getProperty('discgolf') === null ) {var discgolf=""} else { var discgolf='<li>Disc Golf</li>'}
+      if(event.feature.getProperty('dogpark') === null ) {var dogpark=""} else { var discgolf='<li>Dog Park</li>'}
+      if(event.feature.getProperty('golf') === null ) {var golf=""} else { var golf='<li>Golf Course</li>'}
+      if(event.feature.getProperty('bldg') === null ) {var bldg=""} else { var bldg='<li>Neighborhood Building</li>'}
+      if(event.feature.getProperty('openfields') === null ) {var openfields=""} else { var openfields='<li>Open Fields</li>'}
+      if(event.feature.getProperty('playground') === null ) {var playground=""} else { var playground='<li>Playground</li>'}
+      if(event.feature.getProperty('restroom') === null ) {var restroom=""} else { var restroom='<li>Restrooms</li>'}
+      if(event.feature.getProperty('swimming') === null ) {var swimming=""} else { var swimming='<li>Swimming Pool</li>'} 
+      if(event.feature.getProperty('basketball') === null ) {var basketball=""} else { var basketball='<li>Basketball Courts: ' + event.feature.getProperty('basketball') + '</li>'}
+      if(event.feature.getProperty('baseball') === null ) {var baseball=""} else {var baseball='<li>Baseball Fields: ' + event.feature.getProperty('baseball') + '</li>'}
+      if(event.feature.getProperty('tennis') === null ) {var tennis=""} else {var tennis='<li>Tennis Courts: ' + event.feature.getProperty('tennis') + '</li>'}
+      if(event.feature.getProperty('volleyball') === null ) {var volleyball=""} else {var volleyball='<li>Volleyball Courts: ' + event.feature.getProperty('volleyball') + '</li>'}
+      if(event.feature.getProperty('football') === null ) {var football=""} else {var football='<li>Football Fields: ' + event.feature.getProperty('football') + '</li>'}  
+      if(event.feature.getProperty('shelter') === null ) {var shelter=""} else {var shelter='<li>Shelters: ' + event.feature.getProperty('shelter') + '</li>'}
+      if(event.feature.getProperty('pavedtrails') === null ) {var pavedtrails=""} else {var pavedtrails='<li>' + event.feature.getProperty('pavedtrails') + ' Miles of Paved Trails</li>'}  
+      if(event.feature.getProperty('horseshoes') === null ) {var horseshoes=""} else {var horseshoes='<li>Horseshoe Courts: ' + event.feature.getProperty('horseshoes') + '</li>'}  
+
       infowindow.setContent(
         '<p class="park-name">' + event.feature.getProperty('name') + '</p>' +
         '<p class="park-address">' + event.feature.getProperty('address') + '</p>' +
-        '<ul class="feature-list">' + basketball + baseball + tennis + volleyball + shelter + '</ul>'
+        '<p class="park-address">' + event.feature.getProperty('acres') + ' Acres</p>' +
+        '<ul class="feature-list">' + pavedtrails + swimming + golf + playground + dogpark + discgolf + center + bldg + openfields + restrooms + 
+        shelters + baseball + basketball + football + horseshoes + tennis + volleyball + 
+        '</ul>'
         );
       infowindow.setPosition(event.latLng)
       infowindow.open(map);
